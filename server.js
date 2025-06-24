32bfda7442188b7183dc055d27c9617deeb833f3
Sruthi.V <Sruthi.V@nestgroup.net>
2025-06-24 10:52:46 +0530

    read and write to json file

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.json()); // for parsing application/json
  const data = require('./assets/jsonData/projecttreedata.json');
   const ncrdata = require('./assets/jsonData/ncrdata.json');
    const ispodata = require('./assets/jsonData/ispodata.json');
+   const filterdata = require('./assets/jsonData/savedfilter.json');
+const fs = require('fs');
 // Mock endpoints
 app.get('/api/users', (req, res) => {
   res.json([
@@ -35,6 +37,47 @@ app.get('/api/getCurrentProjects',(req,res)=>{
 })
 app.get('/api/getISPO',(req,res)=>{
   res.json(ispodata)
+})
+app.get('/api/getISPO',(req,res)=>{
+  res.json(ispodata)
+})
+//add data to json file while clicking on save filter button
+app.post('/api/saveFilter', (req, res) => {
+  fs.readFile('assets/jsonData/savedfilter.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+      console.log(data)
+    let jsondata=JSON.parse(data); //get array length
+    console.log(jsondata.data.length);
+    let count = jsondata.data.length +1;
+    let obj={}; 
+    let filter="Filter "+count; //create filter name
+    obj[filter]=req.body //add some data
+    jsondata.data.push(obj); //push it to data array
+    console.log(jsondata);
+   // json = JSON.stringify(jsondata); //convert it back to json
+    fs.writeFile('assets/jsonData/savedfilter.json', JSON.stringify(jsondata), (err) => {
+    if (err) {  console.error(err);  return; };
+   
+}); 
+    res.json({message:"Filter saved successfully",filterName:filter}); //send response
+}});
+});
+app.get('/api/getFilterList',(req,res)=>{
+  const fs = require('fs');
+ 
+fs.readFile('assets/jsonData/savedfilter.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Failed to read file:', err);
+    return;
+  }
+ 
+  const jsonData = JSON.parse(data);
+   res.json(jsonData);
+});
+
+
 })
 // Start server
 app.listen(port, () => {
